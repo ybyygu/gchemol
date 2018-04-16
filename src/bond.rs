525f257649c86892d85a94f863ee5d8e875d4628
Wenping Guo <ybyygu@gmail.com>
2018-04-16 10:16:10 +0800

    add bond.order() method

--- src/bond.rs
+++ src/bond.rs
@@ -15,15 +15,37 @@ pub enum BondKind {
 
 #[derive(Debug, Clone)]
 pub struct Bond {
-    pub kind: BondKind,
-    pub name: String,
+    pub kind : BondKind,
+    pub name : String,
+    /// set this attribute for arbitrary bond order
+    order    : Option<f64>,
 }
 
 impl Default for Bond {
     fn default() -> Self {
         Bond {
-            kind: BondKind::Single,
-            name: String::default(),
+            order : None,
+            kind  : BondKind::Single,
+            name  : String::default(),
+        }
+    }
+}
+
+impl Bond {
+    /// Return bond order
+    pub fn order(&self) -> f64 {
+        if let Some(order) = self.order {
+            order
+        } else {
+            match self.kind {
+                BondKind::Dummy     => 0.0,
+                BondKind::Partial   => 0.5,
+                BondKind::Single    => 1.0,
+                BondKind::Aromatic  => 1.5,
+                BondKind::Double    => 2.0,
+                BondKind::Triple    => 3.0,
+                BondKind::Quadruple => 4.0,
+            }
         }
     }
 }
