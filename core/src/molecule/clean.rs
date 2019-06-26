cd92b83f7d87152f70dfdd620544f175ef1b0d88
Wenping Guo <ybyygu@gmail.com>
2019-06-26 09:04:17 +0800

    clean: fix dividing by zero error
    rustfmt

--- core/src/molecule/clean.rs
+++ core/src/molecule/clean.rs
@@ -1,4 +1,6 @@
-// [[file:~/Workspace/Programming/gchemol/core/gchemol-core.note::82294367-1b69-4638-a70b-fd8daf02ff3e][82294367-1b69-4638-a70b-fd8daf02ff3e]]
+// bounds
+
+// [[file:~/Workspace/Programming/gchemol-rs/gchemol/core/gchemol-core.note::*bounds][bounds:1]]
 use super::*;
 
 type Bounds = HashMap<(AtomIndex, AtomIndex), f64>;
@@ -15,7 +17,7 @@ fn get_distance_bounds_v1(mol: &Molecule) -> Bounds {
     let nnodes = node_indices.len();
 
     for i in 0..nnodes {
-        for j in (i+1)..nnodes {
+        for j in (i + 1)..nnodes {
             let node_i = node_indices[i];
             let node_j = node_indices[j];
             let atom_i = &mol.graph[node_i];
@@ -33,7 +35,7 @@ fn get_distance_bounds_v1(mol: &Molecule) -> Bounds {
 
             let lij = crij * 0.8;
             let uij = vrij * 0.8;
-            let uij = if uij > crij*1.2 {uij} else {crij*1.2};
+            let uij = if uij > crij * 1.2 { uij } else { crij * 1.2 };
 
             debug_assert!(lij <= uij);
             let dij = atom_i.distance(atom_j);
@@ -42,7 +44,7 @@ fn get_distance_bounds_v1(mol: &Molecule) -> Bounds {
             // or set vdw radius as the lower bound if not bonded
             if let Some(nb) = mol.nbonds_between(node_i, node_j) {
                 if nb == 1 {
-                    if dij >= lij && dij < crij*1.2 {
+                    if dij >= lij && dij < crij * 1.2 {
                         bounds.insert((node_i, node_j), dij);
                         bounds.insert((node_j, node_i), dij);
                     } else {
@@ -76,15 +78,17 @@ fn get_distance_bounds_v1(mol: &Molecule) -> Bounds {
 
     bounds
 }
-// 82294367-1b69-4638-a70b-fd8daf02ff3e ends here
+// bounds:1 ends here
 
-// [[file:~/Workspace/Programming/gchemol/core/gchemol-core.note::2351f71f-246f-4193-85c9-7bbe4a9d7587][2351f71f-246f-4193-85c9-7bbe4a9d7587]]
+// core
+
+// [[file:~/Workspace/Programming/gchemol-rs/gchemol/core/gchemol-core.note::*core][core:1]]
 // the weight between two atoms
 fn get_weight_between(lij: f64, uij: f64, dij: f64) -> f64 {
     debug_assert!(lij <= uij);
 
     let weight = if dij >= lij && dij < uij {
-        0.0
+        1e-4
     } else if dij < lij {
         1.0
     } else {
@@ -109,7 +113,7 @@ impl Molecule {
         let node_indices: Vec<_> = self.graph.node_indices().collect();
         let nnodes = node_indices.len();
 
-        let maxcycle = nnodes*100;
+        let maxcycle = nnodes * 100;
         let mut icycle = 0;
         let ecut = 1E-4;
 
@@ -119,31 +123,48 @@ impl Molecule {
             let mut positions_new = vec![];
             for i in 0..nnodes {
                 let node_i = node_indices[i];
-                let mut pi = self.get_atom(node_i).expect("atom i from node_i").position();
+                let mut pi = self
+                    .get_atom(node_i)
+                    .expect("atom i from node_i")
+                    .position();
                 let mut pi_new = [0.0; 3];
                 let mut wijs = vec![];
                 let npairs = (nnodes - 1) as f64;
                 let mut stress_i = 0.0;
                 for j in 0..nnodes {
                     // skip self-interaction
-                    if i == j {continue};
+                    if i == j {
+                        continue;
+                    };
 
                     let node_j = node_indices[j];
-                    let pj = self.get_atom(node_j).expect("atom j from node_j").position();
+                    let pj = self
+                        .get_atom(node_j)
+                        .expect("atom j from node_j")
+                        .position();
 
                     // current distance
                     let cur_dij = euclidean_distance(pi, pj);
 
                     // lower bound and upper bound for pair distance
-                    let lij = if node_i < node_j {bounds[&(node_i, node_j)]} else {bounds[&(node_j, node_i)]};
+                    let lij = if node_i < node_j {
+                        bounds[&(node_i, node_j)]
+                    } else {
+                        bounds[&(node_j, node_i)]
+                    };
                     // let uij = bounds[&(node_j, node_i)];
-                    let uij = if node_i < node_j {bounds[&(node_j, node_i)]} else {bounds[&(node_i, node_j)]};
+                    let uij = if node_i < node_j {
+                        bounds[&(node_j, node_i)]
+                    } else {
+                        bounds[&(node_i, node_j)]
+                    };
                     // let (lij, uij) = (lij.min(uij), uij.max(lij));
 
                     // ij pair counts twice, so divide the weight
                     // let wij = lij.powi(-4);
                     let wij = get_weight_between(lij, uij, cur_dij);
-                    let wij = 0.5*wij;
+                    dbg!((wij, lij, uij, cur_dij));
+                    let wij = 0.5 * wij;
                     wijs.push(wij);
 
                     // collect position contribution of atom j to atom i
@@ -151,7 +172,7 @@ impl Molecule {
                     let mut fij = [0.0; 3];
                     for v in 0..3 {
                         pi_new[v] += wij * (pj[v] + lij / cur_dij * (pi[v] - pj[v]));
-                        fij[v] = (1.0 - lij/cur_dij)*xij[v];
+                        fij[v] = (1.0 - lij / cur_dij) * xij[v];
                     }
                     stress_i += wij * (cur_dij - lij).powi(2);
                 }
@@ -160,6 +181,7 @@ impl Molecule {
                 let swij: f64 = wijs.iter().sum();
 
                 // FIXME: if all pair weights are zero
+                dbg!(swij);
                 debug_assert!(swij.abs() >= 1e-4);
                 for v in 0..3 {
                     pi_new[v] /= swij;
@@ -180,7 +202,10 @@ impl Molecule {
                 bail!("found invalid number: {:?}", stress);
             }
 
-            if stress < ecut || (stress - old_stress).abs() < ecut || (stress - old_stress).abs() / stress < ecut{
+            if stress < ecut
+                || (stress - old_stress).abs() < ecut
+                || (stress - old_stress).abs() / stress < ecut
+            {
                 break;
             }
 
@@ -195,4 +220,4 @@ impl Molecule {
         Ok(())
     }
 }
-// 2351f71f-246f-4193-85c9-7bbe4a9d7587 ends here
+// core:1 ends here
